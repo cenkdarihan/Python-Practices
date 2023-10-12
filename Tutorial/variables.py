807499f8cc9bab24b0b5d40eb368217191f39cbc
Cenk Darihan <darihancenk58@gmail.com>
2023-10-12 20:16:04 +0300

    Variables kodlarını gir

---
+++ Tutorial/variables.py
@@ -0,0 +1,38 @@
+# Creating Variables
+
+x = 5
+y = "John"
+print(x)
+print(y) # 5 
+         # John
+         
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+            # Sally
+            
+# Casting
+
+x = str(3)    # if we print x, it will be '3'
+y = int(3)    # if we print y, it will be 3
+z = float(3)  # if we print z, it will be 3.0
+
+# Get the Type
+
+x = 5
+y = "John"
+print(type(x))  # class'int'
+print(type(y))  # class'str'
+
+# Single or Double Quotes?
+
+x = "John"
+# is the same as
+x = 'John'           # İf we print x, it prints "John" in two situation.
+
+# Case-Sensitive
+
+a = 4
+A = "Sall"  # A will not overwrite a
+
+
