a79a8f1f8c3034c18a6af5bd8776aa6f56360a75
Efe Can Kara <efecan_kara@proton.me>
2023-10-12 20:29:35 +0300

    Write the code of numbers.

---
+++ Tutorial/numbers.py
@@ -0,0 +1,39 @@
+# Python Numbers
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+print(type(x))  # Outputs <class 'int'>
+print(type(y))  # Outputs <class 'Float'>
+print(type(z))  # Outputs <class 'complex'>
+
+# Integer
+
+x = 1
+y = 35656222554887711
+z = -3255522
+
+# Float 
+
+x = 1.10
+y = 1.0
+z = -87.7e100
+
+# Complex 
+
+x = 3+5j
+y = 5j
+z = -5j
+
+# Type Conversion
+
+a = float(x)        # a as a float of x
+b = int(y)          # b as an integer of y
+c = complex(z)      # c as a complex of z
+
+# Random Number
+
+import random
+
+print(random.randrange(1, 10)) # Prints a number between 1 and 10
