b683bf92f24d17cf7b47d46af773581677cd2006
Efe Can Kara <efecan_kara@proton.me>
2023-10-12 20:10:32 +0300

    Write the code of Data Types

---
+++ Tutorial/dataTypes.py
@@ -0,0 +1,34 @@
+# Setting the Data Type
+
+x = "Hello World"                               # String
+x = 20                                          # Integer
+x = 20.5                                        # Float
+x = 1j                                          # Complex
+x = ["apple", "banana", "cherry"]               # List
+x = ("apple", "banana", "cherry")               # Tuple
+x = range(6)                                    # Range
+x = {"name" : "John", "age" : 36}               # Dictionary
+x = {"apple", "banana", "cherry"}               # Set
+x = frozenset({"apple", "banana", "cherry"})    # Frozen Set
+x = True                                        # Boolean
+x = b"Hello"                                    # Bytes
+x = bytearray(5)                                # Bytearray
+x = memoryview(bytes(5))                        # Memory View
+x = None                                        # None Type
+
+# Setting the Specific Data Type
+
+x = str("Hello World")
+x = int(20)
+x = float(20.5)
+x = complex(1j)
+x = list(("apple", "banana", "cherry"))
+x = tuple(("apple", "banana", "cherry"))
+x = range(6)
+x = dict(name="John", age=36)
+x = set(("apple", "banana", "cherry"))
+x = frozenset(("apple", "banana", "cherry"))
+x = bool(5)
+x = bytes(5)
+x = bytearray(5)
+x = memoryview(bytes(5))
